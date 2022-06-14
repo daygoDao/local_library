e9231c538a8aec63602f78393f482d44fad9daf6
dao vo <gary.vo32@gmail.com>
2022-06-13 18:56:20 -0700

    almost done implemeneting bookinstance create form

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ var app = express();
 //set up mongoose connection
 var mongoose = require("mongoose");
 var mongoDB =
-  "mongodb+srv://daygoJabroni:nDvgsP7KOBpEGiVM@cluster0.giwn2.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connect errrr:"));
