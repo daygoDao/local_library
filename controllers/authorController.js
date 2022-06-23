04b4a1e589cd5ce0df1c308d66b38bcf4fcca1f2
dao vo <gary.vo32@gmail.com>
2022-06-22 18:37:16 -0700

    able to update POST an author

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -203,6 +203,60 @@ exports.author_update_get = function (req, res, next) {
 };
 
 // Handle Author update on POST.
-exports.author_update_post = function (req, res) {
-  res.send("NOT IMPLEMENTED: Author update POST");
-};
+exports.author_update_post = [
+  // validate and sanitize fields.
+  body("first_name")
+    .trim()
+    .isLength({ min: 2 })
+    .escape()
+    .withMessage("First name must be specified.")
+    .isAlphanumeric()
+    .withMessage("First name non-alphanumeric characters."),
+  body("family_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Family name must be specified")
+    .isAlphanumeric()
+    .withMessage("Family name has non-alphanumeric characters."),
+  body("date_of_birth", "Invalid date of birth")
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+  body("date_of_death", "Invalid date of death")
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+  //process request after validationand sanitization
+  (req, res, next) => {
+    //extract the validation errors form a request
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // there are errors. render form again with sanitized values/errors messages.
+      res.render("author_form", {
+        title: "Update Author",
+        author: req.body,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // data from form is valid.
+      // create an author object with escaped and trimmed data.
+      const author = new Author({
+        first_name: req.body.first_name,
+        family_name: req.body.family_name,
+        date_of_birth: req.body.date_of_birth,
+        date_of_death: req.body.date_of_death,
+        _id: req.params.id,
+      });
+      Author.findByIdAndUpdate(req.params.id, author, {}, function (err, results) {
+        if (err) {
+          return next(err);
+        }
+        //success - redirect to new author record.
+        res.redirect(results.url);
+      });
+    }
+  },
+];
